5c927046f32590f287271bafe29a0289a89f6dbd
crschlosser <carlos.schlosser@yahoo.com.br>
2024-09-15 11:25:01 -0300

    ENTREGA DESAFIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Com NOKIA:");
+Smartphone nokia = new Nokia(numero:"66669999", modelo: "TIJOLAO-1989", imei: "XPTO8886665", memoria:64); 
+nokia.Ligar();
+nokia.InstalarAplicativo("freeshop");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Com Uai-fone:");
+Smartphone iphone = new Iphone(numero:"22233344", modelo: "maçã podre", imei: "xmerdoulada", memoria:8); 
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Nutelafree");
+
+
